80f322105f3fb9d864c31f98f100580af5f6ab27
Danny Tran <dannytran3@gmail.com>
2020-08-31 01:04:03 -0400

    site revision

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import fooString from "./foo.js";
-import bar from "./bar-cjs";
-import img from "./images/dt_profile.jpg";
-import img2 from "./images/black_noise.jpg";
-import "./sass/index.scss";
